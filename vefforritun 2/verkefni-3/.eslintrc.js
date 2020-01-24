b04f2c2f33b5a59c1cf35b5eaaa7867fb721d824
nachos5 <gnorland93@gmail.com>
2020-01-24 13:44:16 +0000

    gitignore clean

--- vefforritun 2/verkefni-3/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: 'airbnb-base',
-  rules: {
-    'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
-    'linebreak-style': 0,
-  },
-  plugins: ['import'],
-};
